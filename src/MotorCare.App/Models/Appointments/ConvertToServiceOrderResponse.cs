93eec0d55046a1a553baacd1356c67a8aa6bd8aa
yigit.cevik <yigitcevik@icloud.com>
2026-04-19 20:19:17 +0300

    randevu detail

--- src/MotorCare.App/Models/Appointments/ConvertToServiceOrderResponse.cs
+++ src/MotorCare.App/Models/Appointments/ConvertToServiceOrderResponse.cs
@@ -3,4 +3,21 @@ namespace MotorCare.App.Models.Appointments;
 public class ConvertToServiceOrderResponse
 {
     public Guid ServiceOrderId { get; set; }
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+
+    public Guid ResolveServiceOrderId()
+    {
+        if (ServiceOrderId != Guid.Empty)
+        {
+            return ServiceOrderId;
+        }
+
+        if (Id != Guid.Empty)
+        {
+            return Id;
+        }
+
+        return OrderId;
+    }
 }
